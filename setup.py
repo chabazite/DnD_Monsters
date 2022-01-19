5adc5392c9965f794997fcfccb06bf11702c32f1
chabazite <ingallsandrew0@gmail.com>
2022-01-18 19:01:15 -0600

    initial commit. Got a dictionary of soupy information and monsters!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scrapping DnD Monsters for EDL and ML insights',
+    author='Andrew Ingalls',
+    license='MIT',
+)
