6d5cc7546d58f2c7874b3c8a18d49f36c5fa386d
chabazite <ingallsandrew0@gmail.com>
2024-01-05 00:53:09 -0600

    updated to use on heroku

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
-import numpy as np
-import tensorflow as tf
-from keras.models import load_model
+import dash
+import dash_bootstrap_components as dbc
 import dash
 import pandas as pd
 import plotly.express as px
@@ -12,49 +10,11 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 
-
-
 app = dash.Dash(
     __name__,
    external_stylesheets=[dbc.themes.DARKLY],
 )
 
-model = load_model('monster_generator.h5')
-
-#empty one-hot encoded arrays for the prediction
-
-dataframe_columns = ['Hit Points','Armor Class','Proficiency Bonus','STR','DEX','CON','WIS','INT','CHA', 'STR_SV','DEX_SV','CON_SV','WIS_SV','INT_SV','CHA_SV', 'Attack_Bonus','Average_Damage_per_Round','Legendary Actions', 'Damage Resistances', 'Damage Immunities', 'Condition Immunities', 'Damage Vulnerabilities', 'Legendary Resistance', 'Magic Resistance']
-
-
-
-def create_prediction_array(level,size,environment,type,alignment,difficulty):
-    '''
-    inputs are from the app. These inputs will "turn on" on of the one-hot encoded columns in the case of environment, type, alignment. For challenge rating it will take the party level and add the difficulty factor
-    size is a ordinal category from 1-6.
-
-    All of these will be concatinated to form the final prediction array. 
-    '''
-    environ = [0,0,0,0,0,0,0,0,0,0,0,0]
-    m_type =  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    m_alignment = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    
-    environ[environment] = 1
-    m_type[type] = 1
-    m_alignment[alignment] = 1
-    challenge_r = level + difficulty
-    cs_array = [challenge_r,size]
-
-    final_array = np.concatenate((cs_array,environ,m_type,m_alignment))
-   
-    return  np.reshape(final_array,(1,44))
-
-def prediction(array):
-    pred = model.predict(array)
-
-    # create dataframe from predictions
-    monster_df = pd.DataFrame(data = pred.astype(int),columns=dataframe_columns)      
-    
-    return monster_df
 
 # build component parts
 stats_graph = dcc.Graph(id ="stats_graph")
@@ -113,7 +73,8 @@ sidebar =  html.Div([
                             {'label': "Mountain", 'value': 6},
                             {'label': "NA", 'value': 7},        
                             {'label': "Swamp", 'value': 8},
-                            {'label': "Underdark", 'value': 9 },          {'label': "Underwater", 'value': 10},
+                            {'label': "Underdark", 'value': 9 },          
+                            {'label': "Underwater", 'value': 10},
                             {'label': "Urban", 'value': 11},]
                     ),
                     html.Br(),
@@ -160,7 +121,8 @@ sidebar =  html.Div([
                             {'label': "Chaotic Good", 'value': 6},
                             {'label': "Chaotic Neutral", 'value': 7},        
                             {'label': "Lawful Evil", 'value': 8},
-                            {'label': "Lawful Good", 'value': 9 },          {'label': "Lawful Neutral", 'value': 10},
+                            {'label': "Lawful Good", 'value': 9 },          
+                            {'label': "Lawful Neutral", 'value': 10},
                             {'label': "True Neutral", 'value': 11},
                             {'label': "Neutral Evil", 'value': 12},
                             {'label': "Neutral Good", 'value': 13},
@@ -214,6 +176,7 @@ sidebar_right = html.Div( [dbc.Row([Attack_gauge]),
                          ]),
                        ])
 
+
 app.layout = html.Div([
                 
                 #Header row that includes title and help button column              
@@ -236,97 +199,6 @@ app.layout = html.Div([
                 ]),
 ])
 
-@app.callback(
-            [Output(component_id='stats_graph', component_property='figure'),
-             Output(component_id='SV_graph', component_property='figure'),
-             Output(component_id='HP', component_property='value'),
-             Output(component_id='AC', component_property='value'),
-             Output(component_id='AD', component_property='value'),
-             Output(component_id='AB', component_property='value'),
-             Output(component_id='leg_ac', component_property='on'),
-             Output(component_id='leg_res', component_property='on'),
-             Output(component_id='mag_res', component_property='on'),
-             Output(component_id='dam_res', component_property='children'),
-             Output(component_id='dam_imm', component_property='children'),
-             Output(component_id='dam_vul', component_property='children'),
-             Output(component_id='cond_imm', component_property='children')],
-                [Input(component_id="level-slider", component_property='value'),
-                 Input(component_id='size', component_property='value'),
-                 Input(component_id='environment', component_property='value'),
-                 Input(component_id='type', component_property='value'),
-                 Input(component_id='alignment', component_property='value'),
-                 Input(component_id='difficulty', component_property='value')
-                 ])
-def get_graph(level,size,environment,type,alignment,difficulty):
 
-    monster_df= prediction(
-        create_prediction_array(level,size,environment,type,alignment,difficulty))
-
-    df = pd.DataFrame(dict(r = list(monster_df.iloc[0].to_numpy()[3:9]),  
-    theta = list(monster_df.columns[3:9])))
-
-    df_sv = pd.DataFrame(dict(r = list(monster_df.iloc[0].to_numpy()[9:15]),  
-    theta = list(monster_df.columns[9:15])))
-
-    #Radar graph stats
-    stats_graph = px.line_polar(df, r='r', theta='theta', line_close=True)
-    stats_graph.update_traces(fill='toself')
-    stats_graph.update_layout(
-        template='plotly_dark',
-        polar=dict(radialaxis=dict(visible=True,range=[0,30])),paper_bgcolor='rgba(0,0,0,0)',
-        title = {
-            'text': "Monster Stats",
-            'y':1,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'
-            },
-            title_font_color = 'white',
-            hovermode='x')
-
-    #Radar graph for saving throws
-    SV_graph = px.line_polar(df_sv, r='r', theta='theta', line_close=True)
-    SV_graph.update_traces(fill='toself')
-    SV_graph.update_layout(
-        template='plotly_dark',
-        polar=dict(radialaxis=dict(visible=True,range=[0,20])),     paper_bgcolor='rgba(0,0,0,0)', 
-        title = {
-            'text': "Saving Throws",
-            'y':1,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'
-            },
-        title_font_color = 'white')
-
-    # stats
-    hit_points = monster_df.loc[0,'Hit Points']
-    armor_class = monster_df.loc[0,'Armor Class']
-    avg_damage = monster_df.loc[0,'Average_Damage_per_Round']
-    attack_bonus = monster_df.loc[0,'Attack_Bonus']
-
-    #Check Marks
-    if monster_df.loc[0,'Legendary Actions'] == 1:
-        leg_actions = True
-    else:
-        leg_actions = False
-
-    if monster_df.loc[0,'Legendary Resistance'] == 1:
-        leg_resist = True
-    else:
-        leg_resist = False
-    
-    if monster_df.loc[0,'Magic Resistance'] == 1:
-        mag_resist = True
-    else:
-        mag_resist = False
-    #numbers
-    dam_res = monster_df.loc[0,'Damage Resistances']
-    dam_immun = monster_df.loc[0,'Damage Immunities']
-    dam_vuln = monster_df.loc[0,'Damage Vulnerabilities']
-    cond_immun = monster_df.loc[0,'Condition Immunities']  
-
-    return stats_graph,SV_graph,hit_points,armor_class,avg_damage, attack_bonus, leg_actions, leg_resist, mag_resist, dam_res, dam_immun, dam_vuln, cond_immun
-
-#if __name__ =='__main__':
-#    app.run_server(debug=True)
+if __name__ =='__main__':
+    app.run_server(debug=True)
